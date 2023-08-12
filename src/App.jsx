ac0731e8b9bbe6e4313de208a923c7c40b0e0121
William Dias <smdwilliam89@gmail.com>
2023-08-12 15:21:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import './index.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Sunny-side</h1>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'soft-red': 'hsl(7, 99%, 70%)',
+        yellow: 'hsl(51, 100%, 49%)',
+        'design-text': 'hsl(167, 40%, 24%)',
+        'dark-blue': 'hsl(198, 62%, 26%)',
+        footer: 'hsl(168, 34%, 41%)',
+      },
+      color: {
+        'very-dark-desatured-blue': 'hsl(212, 27%, 19%)',
+        'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+        'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+        'grayish-blue': 'hsl(210, 4%, 67%)',
+        white: 'hsl(0, 0%, 100%)',
+      },
+    },
+  },
+  plugins: [],
+};
